1952fee2ba948a97b0a1c8c07cddbc53de887a78
춤추는망고 <ensia96@gmail.com>
2021-01-05 17:58:33 +0900

    [change] meta config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `춤추는 블로그`,
+  description: `춤추는 망고의 기술 블로그`,
+  author: `춤추는 망고`,
+  introduction: `하하호호`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `ensia96`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    facebook: `ensia96`, // Your Facebook account
+    linkedin: `ensia96`, // Your LinkedIn account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/fav.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: "", // Your disqus-short-name. check disqus.com.
+    utterances: "ensia96/ensia96.github.io", // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: "jbee",
   },
   share: {
-    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+    facebookAppId: "", // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
-}
+  ga: "", // Add your google analytics tranking ID
+};

a9d57af2d8f921de3bec194022fbd6ee1464ba3c
춤추는망고 <ensia96@gmail.com>
2021-01-06 21:49:48 +0900

    [change] 404 page component to functional

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,22 +4,20 @@ import { graphql } from 'gatsby'
 import { Layout } from '../layout'
 import { Head } from '../components/head'
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <Head title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    )
-  }
-}
-
-export default NotFoundPage
+export default ({
+  data: {
+    site: {
+      siteMetadata: { title: siteTitle },
+    },
+  },
+  location,
+}) => (
+  <Layout location={location} title={siteTitle}>
+    <Head title="404: Not Found" />
+    <h1>Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
+)
 
 export const pageQuery = graphql`
   query {

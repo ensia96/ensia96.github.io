45bdb557e35307047c449c092a1141276ffcab1d
춤추는망고 <ensia96@gmail.com>
2021-01-07 22:30:51 +0900

    maintanance/style/config (#16)

---
+++ src/styles/variable.js
@@ -0,0 +1,61 @@
+const style = {
+  app: {
+    themeGradient: 'linear-gradient(72deg, #291e95, #cc007a);',
+    topHeaderTextColor: '#fff;',
+  },
+  light: {
+    body: {
+      backgroundColor: '#fff;',
+      textShadow: '0 0 0.1px rgba(0, 0, 0, 0.3);',
+      hoverTextShadow: '0 0 6px rgba(0, 0, 0, 0.2);',
+    },
+    index: {
+      dimmedColor: '#eef5db;',
+      linkColor: '#000080;',
+      darkGray: '#333;',
+      middleGray: '#7d7d7d;',
+      lightGray: '#aaa;',
+    },
+    blogTemplates: {
+      navigatorTextColor: '#cc007a;',
+      navigatorBackgroundColor: '#fceff7;',
+      blockquoteTextColor: '#999;',
+    },
+    code: {
+      inlineDimmedColor: '#ece5f1;',
+      inlineTextColor: '#4b2043;',
+    },
+    category: {
+      categoryBorderColor: '#ecf0f2;',
+      categoryHighlightBorderColor: '#909da1;',
+      categoryBackgroundColor: '#f4f7f8;',
+      categoryTextColor: '#666;',
+      categoryHighlightTextColor: '#636c6e;',
+      categoryItemColor: '#fff;',
+    },
+  },
+}
+
+const dummy = `
+/**
+ * @dark theme
+ */
+$dark-background-color: #282c35;
+$dark-link-color: #bb72ec;
+
+$dark-lightest-font-color: #fff;
+$dark-light-font-color: #eeeeee;
+$dark-middle-font-color: #d8d7d7;
+
+$dark-text-shadow: 0 0 0.1px rgba(255, 255, 255, 0.3);
+$dark-hover-text-shadow: 0 0 6px rgba(255, 255, 255, 0.2);
+
+$dark-category-item-color: #282c35;
+$dark-category-background-color: #24272c;
+
+$dark-category-border-color: rgb(56, 54, 54);
+$dark-category-text-color: #d8d7d7;
+
+$dark-category-highlight-border-color: #666;
+$dark-category-highlight-text-color: #fff;
+`

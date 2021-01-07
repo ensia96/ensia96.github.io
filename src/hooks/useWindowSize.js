326e7bbbc2f2717b1fc5f0bb0c2973fed65dd586
춤추는망고 <ensia96@gmail.com>
2021-01-07 23:36:58 +0900

    [add] default value setting in use window size hook

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react'
 
+let defaultWidth
+
+if (typeof window !== `undefined`) {
+  defaultWidth = window.innerWidth
+}
+
 export default () => {
-  const [windowSize, setWindowSize] = useState({ width: window.innerWidth })
+  const [windowSize, setWindowSize] = useState({ width: defaultWidth })
 
   const resize = () => setWindowSize({ width: window.innerWidth })
 

f730f58559a230e2a1def41ea520212254d5c62e
춤추는망고 <ensia96@gmail.com>
2021-01-07 21:38:38 +0900

    [create] main frame component

---
+++ src/layout/main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default styled.div`
+  margin-top: 70px;
+
+  @media all and (min-width: 992px) {
+    margin-top: 50px;
+    margin-left: 300px !important;
+  }
+`

b72f8b36bcd629689fc1706ab3bbe3316265d696
춤추는망고 <ensia96@gmail.com>
2021-01-07 21:39:23 +0900

    [create] green dot component

---
+++ src/layout/greendot.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default styled.div`
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  background-color: #49c39e;
+  vertical-align: middle;
+  margin: 0px 10px;
+`

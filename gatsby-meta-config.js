bd1f42474cad6090d0ba22a0584d0bd4698772c4
춤추는망고 <ensia96@gmail.com>
2021-01-10 00:17:46 +0900

    [change] config of gatsby

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `춤추는 기술 블로그`,
+  description: `컴퓨터처럼 생각하고, 글쓰듯 코딩하기`,
+  author: `춤추는 망고`,
+  introduction: `아름다운 코드를 추구하고, 배움과 소통을 사랑하는 백엔드 개발자, 춤추는 망고입니다 !`,
+  siteUrl: `https://ensia96.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `ensia96`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    linkedin: `ensia96`, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: '', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2

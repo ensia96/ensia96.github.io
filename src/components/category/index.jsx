94597d537eb76bb1dd17d9173eaa30a221242ef6
춤추는망고 <ensia96@gmail.com>
2021-01-09 21:59:12 +0900

    [delete] unnecessary actions & functions

--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -1,73 +1,47 @@
-import React, { useCallback, useRef, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import { StaticQuery } from 'gatsby'
 import { rhythm } from '../../utils/typography'
 import './index.scss'
 import { Item } from './item'
 
-export const Category = ({ category, selectCategory }) => {
-  const containerRef = useRef(null)
-
-  const scrollToCenter = useCallback(
-    tabRef => {
-      const { offsetWidth: tabWidth } = tabRef.current
-      const { scrollLeft, offsetWidth: containerWidth } = containerRef.current
-      const tabLeft = tabRef.current.getBoundingClientRect().left
-      const containerLeft = containerRef.current.getBoundingClientRect().left
-      const refineLeft = tabLeft - containerLeft
-      const targetScollX =
-        scrollLeft + refineLeft - containerWidth / 2 + tabWidth / 2
-
-      containerRef.current.scroll({
-        left: targetScollX,
-        top: 0,
-        behavior: 'smooth',
-      })
-    },
-    [containerRef]
-  )
-
-  return (
-    <StaticQuery
-      query={categoryQuery}
-      render={({ allMarkdownRemark: { edges } }) => {
-        const categories = useMemo(
-          () =>
-            Array.from(
-              new Set(edges.map(({ node }) => node.frontmatter.category))
-            ),
-          []
-        )
-        return (
-          <ul
-            ref={containerRef}
-            className="category-container"
-            role="tablist"
-            id="category"
-            style={{
-              margin: `0 -${rhythm(3 / 4)}`,
-            }}
-          >
+export const Category = ({ category, selectCategory }) => (
+  <StaticQuery
+    query={categoryQuery}
+    render={({ allMarkdownRemark: { edges } }) => {
+      const categories = useMemo(
+        () =>
+          Array.from(
+            new Set(edges.map(({ node }) => node.frontmatter.category))
+          ),
+        []
+      )
+      return (
+        <ul
+          className="category-container"
+          role="tablist"
+          id="category"
+          style={{
+            margin: `0 -${rhythm(3 / 4)}`,
+          }}
+        >
+          <Item
+            title={'All'}
+            selectedCategory={category}
+            onClick={selectCategory}
+          />
+          {categories.map((title, idx) => (
             <Item
-              title={'All'}
+              key={idx}
+              title={title}
               selectedCategory={category}
               onClick={selectCategory}
-              scrollToCenter={scrollToCenter}
             />
-            {categories.map((title, idx) => (
-              <Item
-                key={idx}
-                title={title}
-                selectedCategory={category}
-                onClick={selectCategory}
-                scrollToCenter={scrollToCenter}
-              />
-            ))}
-          </ul>
-        )
-      }}
-    />
-  )
-}
+          ))}
+        </ul>
+      )
+    }}
+  />
+)
 
 const categoryQuery = graphql`
   query {

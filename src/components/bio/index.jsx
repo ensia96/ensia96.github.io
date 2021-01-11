87a429b66b03166271c953eb5d7840eaecdf3798
춤추는망고 <ensia96@gmail.com>
2021-01-11 22:23:11 +0900

    [delete] container component definition & add importation

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -3,23 +3,7 @@ import styled from 'styled-components'
 import { StaticQuery, graphql, Link } from 'gatsby'
 
 import NameShake from './nameshake.js'
-
-const Container = styled.div`
-  visibility: ${props => (props.show ? 'visiable' : 'hidden')};
-  opacity: ${props => (props.show ? '1' : '0')};
-  transition: visibility 0.12s linear, opacity 0.12s linear;
-  display: block;
-  position: fixed;
-  left: 1rem;
-  top: 6rem;
-  width: 243px;
-  z-index: 3;
-  border-radius: 2%;
-  border: 1px solid #dddddd;
-  background-color: #eeeeee;
-  margin: 3px;
-  padding: 10px;
-`
+import Container from './container.js'
 
 const OutSide = styled.div`
   visibility: ${props => (props.show ? 'visiable' : 'hidden')};

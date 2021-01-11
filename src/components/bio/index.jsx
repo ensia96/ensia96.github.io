6889871f7fab7e81e72c8e04a40ae7e954efbd1a
춤추는망고 <ensia96@gmail.com>
2021-01-11 23:46:37 +0900

    [delete] unnecessary importation

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { StaticQuery, graphql, Link } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby'
 
 import NameShake from './nameshake.js'
 import Container from './container.js'

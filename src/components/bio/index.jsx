d880205e7ffc17a76902cec98f7986985da59733
춤추는망고 <ensia96@gmail.com>
2021-01-11 22:17:38 +0900

    [add] shaking style to name shake component

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -10,6 +10,47 @@ const NameShake = styled(Link)`
   border-radius: 8px;
   transform-origin: center;
   animation: flutter 2s infinite linear;
+  @-webkit-keyframes flutter {
+    0% {
+      transform: rotate(0deg);
+    }
+    35% {
+      transform: rotate(0deg);
+    }
+    40% {
+      transform: rotate(-5deg);
+    }
+    60% {
+      transform: rotate(5deg);
+    }
+    65% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+
+  @keyframes flutter {
+    0% {
+      transform: rotate(0deg);
+    }
+    35% {
+      transform: rotate(0deg);
+    }
+    40% {
+      transform: rotate(-5deg);
+    }
+    60% {
+      transform: rotate(5deg);
+    }
+    65% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
 `
 
 const Container = styled.div`

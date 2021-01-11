3980269573bc75563e882929f3b3738f960321a7
춤추는망고 <ensia96@gmail.com>
2021-01-11 23:42:03 +0900

    [change] transition time

--- src/components/bio/container.js
+++ src/components/bio/container.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export default styled.div`
   visibility: ${props => (props.show ? 'visiable' : 'hidden')};
   opacity: ${props => (props.show ? '1' : '0')};
-  transition: visibility 0.12s linear, opacity 0.12s linear;
+  transition: visibility 0.15s linear, opacity 0.15s linear;
   display: block;
   position: fixed;
   left: 1rem;

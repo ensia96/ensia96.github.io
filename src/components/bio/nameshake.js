8b5d95bb6007fb8bd95de69df633fa79905ba690
춤추는망고 <ensia96@gmail.com>
2021-01-11 22:20:07 +0900

    [create] name shake component module

---
+++ src/components/bio/nameshake.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+export default styled(Link)`
+  display: inline-block;
+  font-size: 95%;
+  padding: 2px 6px;
+  font-weight: bolder;
+  border-radius: 8px;
+  transform-origin: center;
+  animation: flutter 2s infinite linear;
+
+  @-webkit-keyframes flutter {
+    0% {
+      transform: rotate(0deg);
+    }
+    35% {
+      transform: rotate(0deg);
+    }
+    40% {
+      transform: rotate(-5deg);
+    }
+    60% {
+      transform: rotate(5deg);
+    }
+    65% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+
+  @keyframes flutter {
+    0% {
+      transform: rotate(0deg);
+    }
+    35% {
+      transform: rotate(0deg);
+    }
+    40% {
+      transform: rotate(-5deg);
+    }
+    60% {
+      transform: rotate(5deg);
+    }
+    65% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+`

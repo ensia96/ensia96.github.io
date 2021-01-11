5d5a39ab8b9dbeb033626e2435c630558f40fd0b
춤추는망고 <ensia96@gmail.com>
2021-01-11 22:14:35 +0900

    [change] transition time

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -18,7 +18,7 @@ const NameShake = styled(Link)`
 const Container = styled.div`
   visibility: ${props => (props.show ? 'visiable' : 'hidden')};
   opacity: ${props => (props.show ? '1' : '0')};
-  transition: visibility 0.15s linear, opacity 0.15s linear;
+  transition: visibility 0.12s linear, opacity 0.12s linear;
   display: block;
   position: fixed;
   left: 1rem;

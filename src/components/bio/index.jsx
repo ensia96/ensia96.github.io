be255d012545ae5d7b7ca39387fdfd27c1b6ab9a
춤추는망고 <ensia96@gmail.com>
2021-01-11 22:20:33 +0900

    [delete] name shake component definition & add importation

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -2,56 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { StaticQuery, graphql, Link } from 'gatsby'
 
-const NameShake = styled(Link)`
-  display: inline-block;
-  font-size: 95%;
-  padding: 2px 6px;
-  font-weight: bolder;
-  border-radius: 8px;
-  transform-origin: center;
-  animation: flutter 2s infinite linear;
-  @-webkit-keyframes flutter {
-    0% {
-      transform: rotate(0deg);
-    }
-    35% {
-      transform: rotate(0deg);
-    }
-    40% {
-      transform: rotate(-5deg);
-    }
-    60% {
-      transform: rotate(5deg);
-    }
-    65% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(0deg);
-    }
-  }
-
-  @keyframes flutter {
-    0% {
-      transform: rotate(0deg);
-    }
-    35% {
-      transform: rotate(0deg);
-    }
-    40% {
-      transform: rotate(-5deg);
-    }
-    60% {
-      transform: rotate(5deg);
-    }
-    65% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(0deg);
-    }
-  }
-`
+import NameShake from './nameshake.js'
 
 const Container = styled.div`
   visibility: ${props => (props.show ? 'visiable' : 'hidden')};

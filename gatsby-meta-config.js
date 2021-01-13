93dd1a3576c1b4178faae5861383a2cc9df2bf25
춤추는망고 <ensia96@gmail.com>
2021-01-13 22:28:14 +0900

    deployment/config/meta-data (#42)

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: `춤추는 기술 블로그`,
-  description: `컴퓨터처럼 생각하고, 글쓰듯 코딩하기`,
+  description: `어서오세요! 춤추는 망고의 기술 블로그입니다!`,
   author: `춤추는 망고`,
-  introduction: `아름다운 코드를 추구하고, 배움과 소통을 사랑하는 백엔드 개발자, 춤추는 망고입니다 !`,
+  introduction: `컴퓨터처럼 생각하고, 말하듯 코딩하기`,
   siteUrl: `https://ensia96.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
@@ -15,7 +15,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: '', // Your repository for archive comment
+    utterances: 'ensia96/ensia96.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
@@ -26,5 +26,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'G-806QP720RD', // Add your google analytics tranking ID
 }

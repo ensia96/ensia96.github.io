06b78368c168b78bca165c751d5cc9c8f186a0e1
춤추는망고 <ensia96@gmail.com>
2021-01-14 20:09:58 +0900

    [create] avatar component

---
+++ src/components/avatar.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+import Image from 'gatsby-image'
+
+export default styled(Image)`
+  width: ${({ size }) => size}px !important;
+  height: ${({ size }) => size}px !important;
+  border-radius: 100% !important;
+  margin-right: 5px !important;
+
+  img {
+    margin-bottom: 0px !important;
+  }
+`

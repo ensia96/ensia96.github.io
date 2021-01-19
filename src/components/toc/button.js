e39ce7be9ec8c5e9750804b1e21d0b03b55a0d3b
춤추는망고 <ensia96@gmail.com>
2021-01-20 00:24:40 +0900

    [add] space for readability

--- src/components/toc/button.js
+++ src/components/toc/button.js
@@ -3,15 +3,21 @@ import styled from 'styled-components'
 export default styled.button`
   z-index: 1;
   position: fixed;
+
   top: 30%;
   right: 200px;
+
   width: 15px;
   height: 50px;
+
   border-top-left-radius: 100%;
   border-bottom-left-radius: 100%;
+
   transition: margin 0.5s;
   margin-right: ${({ open }) => (open ? 0 : '-200px')};
+
   outline: 0;
   border: 1px solid #bbbbbb;
+
   cursor: pointer;
 `

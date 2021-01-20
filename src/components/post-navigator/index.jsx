8f56f0539eaf930b85459928791d01ed4297c7d1
춤추는망고 <ensia96@gmail.com>
2021-01-20 21:12:35 +0900

    [add] destructuring

--- src/components/post-navigator/index.jsx
+++ src/components/post-navigator/index.jsx
@@ -3,25 +3,21 @@ import { Link } from 'gatsby'
 
 import './index.scss'
 
-export const PostNavigator = ({ pageContext }) => {
-  const { previous, next } = pageContext
-
-  return (
-    <ul className="navigator">
-      <li>
-        {previous && (
-          <Link to={previous.fields.slug} rel="prev">
-            ← {previous.frontmatter.title}
-          </Link>
-        )}
-      </li>
-      <li>
-        {next && (
-          <Link to={next.fields.slug} rel="next">
-            {next.frontmatter.title} →
-          </Link>
-        )}
-      </li>
-    </ul>
-  )
-}
+export const PostNavigator = ({ pageContext: { previous, next } }) => (
+  <ul className="navigator">
+    <li>
+      {previous && (
+        <Link to={previous.fields.slug} rel="prev">
+          ← {previous.frontmatter.title}
+        </Link>
+      )}
+    </li>
+    <li>
+      {next && (
+        <Link to={next.fields.slug} rel="next">
+          {next.frontmatter.title} →
+        </Link>
+      )}
+    </li>
+  </ul>
+)

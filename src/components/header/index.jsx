cc264b482fb15df1100852ae834ce4be67608af0
춤추는망고 <ensia96@gmail.com>
2021-01-20 20:40:51 +0900

    [change] configuration of header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 
 import './index.scss'
 
-export const Header = ({ title, isRoot }) =>
+export default ({ title, isRoot }) =>
   isRoot && (
     <h1 className="home-header">
       <Link to={`/`} className="link">
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -6,7 +6,7 @@ import useWindowSize from '../hooks/useWindowSize'
 import Bio from '../components/bio'
 import { ThemeSwitch } from '../components/theme-switch'
 import Footer from '../components/footer'
-import { Header } from '../components/header'
+import Header from '../components/header'
 
 import { Category } from '../components/category'
 

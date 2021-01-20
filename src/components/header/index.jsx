a713802acd94319887d036f86b9855fc3b48d4aa
춤추는망고 <ensia96@gmail.com>
2021-01-20 20:50:21 +0900

    [add] styled h1 component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import styled from 'styled-components'
+
 import './index.scss'
+
+const HomeHeader = styled.h1`
+  margin-top: 0;
+  border-bottom: none;
+  font-weight: 900;
+  font-size: 48px;
+  letter-spacing: -2px;
+`
 
 export default ({ title, isRoot }) =>
   isRoot && (
-    <h1 className="home-header">
+    <HomeHeader>
       <Link to={`/`} className="link">
         {title}
       </Link>
-    </h1>
+    </HomeHeader>
   )

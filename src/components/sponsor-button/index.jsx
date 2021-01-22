5e129922cd3d5dce45daa48da40b790787e29f86
춤추는망고 <ensia96@gmail.com>
2021-01-22 20:08:01 +0900

    [delete] styled component & import components

--- src/components/sponsor-button/index.jsx
+++ src/components/sponsor-button/index.jsx
@@ -1,45 +1,9 @@
 import React from 'react'
 
-import styled from 'styled-components'
-
-const Logo = styled.img`
-  width: 20px;
-  margin-bottom: 1px;
-  box-shadow: none;
-  border: none;
-  vertical-align: middle;
-`
-
-const Content = styled.span`
-  margin-left: 6px;
-`
-
-const Button = styled.a`
-  display: inline-block;
-  padding: 0px 7px;
-  width: 133px;
-  height: 33px;
-  text-decoration: none;
-  background-color: #bb5794;
-  color: #ffffff;
-  border: 1px solid transparent;
-  border-radius: 6px;
-  letter-spacing: -0.08px;
-  box-sizing: border-box;
-  font-size: 12px;
-  line-height: 30px;
-  text-align: left;
-
-  &:hover,
-  &:active {
-    background-color: #a0457d;
-  }
-`
-
-const Container = styled.div`
-  text-align: right;
-  margin: 4px;
-`
+import Container from './container'
+import Button from './button'
+import Logo from './logo'
+import Content from './content'
 
 export default ({ sponsorId }) => (
   <Container>

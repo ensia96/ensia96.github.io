954f9c68c4012ba3c3fef4f752c8ad4b37de1795
춤추는망고 <ensia96@gmail.com>
2021-01-22 23:05:04 +0900

    [create] container component in thumbnail container component

--- src/components/thumbnail-container/index.jsx
+++ src/components/thumbnail-container/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
+import styled from 'styled-components'
+
 import './index.scss'
+
+const Container = styled.div`
+  min-height: calc(100vh - 3.5rem);
+`
 
 export default React.memo(({ children }) => (
   <div className="thumbnail-container" children={children} />

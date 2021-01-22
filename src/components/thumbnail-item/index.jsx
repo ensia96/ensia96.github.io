762f13d6645cca956fa21fb528757673015655ca
춤추는망고 <ensia96@gmail.com>
2021-01-22 23:29:46 +0900

    [add] destructuring

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -4,11 +4,17 @@ import { TARGET_CLASS } from '../../utils/visible'
 
 import './index.scss'
 
-export default ({ node }) => (
-  <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-    <div key={node.fields.slug}>
-      <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+export default ({
+  node: {
+    fields: { slug },
+    frontmatter: { title },
+    excerpt,
+  },
+}) => (
+  <Link className={`thumbnail ${TARGET_CLASS}`} to={slug}>
+    <div key={slug}>
+      <h3>{title || slug}</h3>
+      <p dangerouslySetInnerHTML={{ __html: excerpt }} />
     </div>
   </Link>
 )

43b67cde34c5870ed0a871a3cfc27bda9b42e0b0
춤추는망고 <ensia96@gmail.com>
2021-01-22 23:03:11 +0900

    [simplify] component configuration

--- src/components/thumbnail-container/index.jsx
+++ src/components/thumbnail-container/index.jsx
@@ -3,5 +3,5 @@ import React from 'react'
 import './index.scss'
 
 export default React.memo(({ children }) => (
-  <div className="thumbnail-container">{children}</div>
+  <div className="thumbnail-container" children={children} />
 ))

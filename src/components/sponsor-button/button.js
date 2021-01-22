61bbd87184af0af8da6c06382358e9c703fbba38
춤추는망고 <ensia96@gmail.com>
2021-01-22 20:05:34 +0900

    [create] button component module

---
+++ src/components/sponsor-button/button.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export default styled.a`
+  display: inline-block;
+  padding: 0px 7px;
+  width: 133px;
+  height: 33px;
+  text-decoration: none;
+  background-color: #bb5794;
+  color: #ffffff;
+  border: 1px solid transparent;
+  border-radius: 6px;
+  letter-spacing: -0.08px;
+  box-sizing: border-box;
+  font-size: 12px;
+  line-height: 30px;
+  text-align: left;
+
+  &:hover,
+  &:active {
+    background-color: #a0457d;
+  }
+`

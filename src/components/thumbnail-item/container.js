9398980f7a261048f6815b23d8625867ee0aa11d
춤추는망고 <ensia96@gmail.com>
2021-01-23 18:52:59 +0900

    [create] container component module

---
+++ src/components/thumbnail-item/container.js
@@ -0,0 +1,18 @@
+import { Link } from 'gatsby'
+
+import styled from 'styled-components'
+
+export default styled(Link)`
+  display: block;
+  margin-bottom: 12px;
+  padding: 4px;
+  padding-bottom: 12px;
+  box-shadow: none;
+  transition: text-shadow 0.3s, opacity 0.4s;
+  opacity: 0;
+
+  p {
+    font-size: 90%;
+    line-height: 1.4;
+  }
+`

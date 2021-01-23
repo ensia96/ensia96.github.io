99ca3aea5562740a8995bc6065889caaf2c35f62
춤추는망고 <ensia96@gmail.com>
2021-01-23 18:50:28 +0900

    [create] container styled component

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
 
+import styled from 'styled-components'
+
 import './index.scss'
+
+const Container = styled(Link)`
+  display: block;
+  margin-bottom: 12px;
+  padding: 4px;
+  padding-bottom: 12px;
+  box-shadow: none;
+  transition: text-shadow 0.3s, opacity 0.4s;
+  opacity: 0;
+
+  p {
+    font-size: 90%;
+    line-height: 1.4;
+  }
+`
 
 export default ({
   node: {

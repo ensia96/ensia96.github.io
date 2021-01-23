c75e4e7bd344ab891d5e9c5f8ddda971de6819e1
춤추는망고 <ensia96@gmail.com>
2021-01-23 18:53:33 +0900

    [delete] unnecessary declaration & change rendering configuration

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,25 +1,9 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
 
-import styled from 'styled-components'
+import Container from './container'
 
 import './index.scss'
-
-const Container = styled(Link)`
-  display: block;
-  margin-bottom: 12px;
-  padding: 4px;
-  padding-bottom: 12px;
-  box-shadow: none;
-  transition: text-shadow 0.3s, opacity 0.4s;
-  opacity: 0;
-
-  p {
-    font-size: 90%;
-    line-height: 1.4;
-  }
-`
 
 export default ({
   node: {

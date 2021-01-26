ba7c80a8437efdba40b5e7d7bdc0f1185a347473
춤추는망고 <ensia96@gmail.com>
2021-01-26 22:52:36 +0900

    [change] process related codes in theme switch

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -1,76 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Switch from 'react-switch'
-
-import * as Dom from '../../utils/dom'
-import * as Storage from '../../utils/storage'
-import { THEME } from '../../constants'
 
 import './index.scss'
 
 import Icon from './icon'
 
-function getTheme(checked) {
-  return checked ? THEME.DARK : THEME.LIGHT
-}
-
-function toggleTheme(theme) {
-  switch (theme) {
-    case THEME.LIGHT: {
-      Dom.addClassToBody(THEME.LIGHT)
-      Dom.removeClassToBody(THEME.DARK)
-      break
-    }
-    case THEME.DARK: {
-      Dom.addClassToBody(THEME.DARK)
-      Dom.removeClassToBody(THEME.LIGHT)
-      break
-    }
-  }
-}
-
-// 작업 예정
-export default () => {
-  const [checked, setChecked] = useState(false)
-
-  const handleChange = checked => {
-    const theme = getTheme(checked)
-
-    Storage.setTheme(checked)
-    setChecked(checked)
-    toggleTheme(theme)
-  }
-
-  useEffect(() => {
-    const checked = Storage.getTheme(Dom.hasClassOfBody(THEME.DARK))
-
-    handleChange(checked)
-  }, [])
-
-  return (
-    <div className="switch-container">
-      <label htmlFor="normal-switch">
-        <Switch
-          onChange={handleChange}
-          checked={checked}
-          id="normal-switch"
-          height={24}
-          width={48}
-          checkedIcon={
-            <div className="icon checkedIcon">
-              <Icon type="moon" />
-            </div>
-          }
-          uncheckedIcon={
-            <div className="icon uncheckedIcon">
-              <Icon type="sun" />
-            </div>
-          }
-          offColor={'#d9dfe2'}
-          offHandleColor={'#fff'}
-          onColor={'#999'}
-          onHandleColor={'#282c35'}
-        />
-      </label>
-    </div>
-  )
-}
+export default ({ isDark, setTheme }) => (
+  <div className="switch-container">
+    <label htmlFor="normal-switch">
+      <Switch
+        onChange={() => setTheme(!isDark)}
+        checked={isDark}
+        id="normal-switch"
+        height={24}
+        width={48}
+        checkedIcon={
+          <div className="icon checkedIcon">
+            <Icon type="moon" />
+          </div>
+        }
+        uncheckedIcon={
+          <div className="icon uncheckedIcon">
+            <Icon type="sun" />
+          </div>
+        }
+        offColor={'#d9dfe2'}
+        offHandleColor={'#fff'}
+        onColor={'#999'}
+        onHandleColor={'#282c35'}
+      />
+    </label>
+  </div>
+)

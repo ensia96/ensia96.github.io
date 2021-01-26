93cc23767bf597b16fae35918f4c9a434c94c0aa
춤추는망고 <ensia96@gmail.com>
2021-01-26 22:10:32 +0900

    [add] values to theme object & exporation

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,3 +1,15 @@
-const light = {}
+export const light = {
+  main: '#ffcd00',
+  background: '#ffffff',
+  font: '#333333',
+  tocButton: '#663399',
+}
 
-const dark = {}
+export const dark = {
+  main: '#663399',
+  background: '#1a1c1d',
+  font: '#cccccc',
+  tocButton: '#ffcd00',
+}
+
+//tocButton: '#6e96e6',

d40f40343268aedf57845cfb864624cc88935ae5
춤추는망고 <ensia96@gmail.com>
2021-01-26 23:04:05 +0900

    [change] main color of dark theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,11 +2,11 @@ export const light = {
   main: '#ffcd00',
   background: '#ffffff',
   font: '#333333',
-  tocButton: '#4B1E78',
+  tocButton: '#482c6b',
 }
 
 export const dark = {
-  main: '#4B1E78',
+  main: '#482c6b',
   background: '#272827',
   font: '#cccccc',
   tocButton: '#ffcd00',

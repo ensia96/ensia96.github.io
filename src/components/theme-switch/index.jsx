f868f74e41dde0df53d266c63344bee4d57c3b8a
춤추는망고 <ensia96@gmail.com>
2021-01-28 20:41:11 +0900

    [modify] configuration of theme switch component

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -1,35 +1,20 @@
 import React from 'react'
 import Switch from 'react-switch'
 
-import './index.scss'
-
 import Icon from './icon'
 
 // 작업 예정
 export default ({ theme, toggleTheme }) => (
-  <div className="switch-container">
-    <label htmlFor="normal-switch">
-      <Switch
-        onChange={toggleTheme}
-        checked={theme === 'dark'}
-        id="normal-switch"
-        height={24}
-        width={48}
-        checkedIcon={
-          <div className="icon checkedIcon">
-            <Icon type="moon" />
-          </div>
-        }
-        uncheckedIcon={
-          <div className="icon uncheckedIcon">
-            <Icon type="sun" />
-          </div>
-        }
-        offColor={'#d9dfe2'}
-        offHandleColor={'#fff'}
-        onColor={'#999'}
-        onHandleColor={'#282c35'}
-      />
-    </label>
-  </div>
+  <Switch
+    onChange={toggleTheme}
+    checked={theme === 'dark'}
+    height={24}
+    width={48}
+    checkedIcon={<Icon type="moon" />}
+    uncheckedIcon={<Icon type="sun" />}
+    offColor={'#d9dfe2'}
+    offHandleColor={'#fff'}
+    onColor={'#999'}
+    onHandleColor={'#282c35'}
+  />
 )

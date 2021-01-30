86d3ebfab9e1401cfdc64b6175d61551327aaea7
춤추는망고 <ensia96@gmail.com>
2021-01-30 19:34:00 +0900

    [delete] unnecessary importation

--- src/components/utterances/index.jsx
+++ src/components/utterances/index.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from 'react'
 
-import * as Dom from '../../utils/dom'
-import { THEME } from '../../constants'
-
 const src = 'https://utteranc.es/client.js'
 const branch = 'master'
 const DARK_THEME = 'photon-dark'

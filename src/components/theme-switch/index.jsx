7c3d03c6950b51d39265ed2d7f78f2a98ab41d72
춤추는망고 <ensia96@gmail.com>
2021-02-04 16:59:07 +0900

    [add] code copied from example

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -3,17 +3,44 @@ import Switch from 'react-switch'
 
 import Icon from './icon'
 
-export default ({ theme, toggleTheme }) => (
-  <Switch
-    onChange={toggleTheme}
-    checked={theme === 'dark'}
-    height={24}
-    width={48}
-    checkedIcon={<Icon type="moon" />}
-    uncheckedIcon={<Icon type="sun" />}
-    offColor={'#d9dfe2'}
-    offHandleColor={'#fff'}
-    onColor={'#999'}
-    onHandleColor={'#282c35'}
-  />
-)
+import './index.scss'
+
+export default ({ theme, toggleTheme }) => {
+  console.log('theme : ', theme)
+
+  return (
+    <>
+      <div className="mid">
+        <label className="rocker">
+          <input type="checkbox" />
+          <span className="switch-left">On</span>
+          <span className="switch-right">Off</span>
+        </label>
+      </div>
+      <div className="mid">
+        <label className="rocker rocker-small">
+          <input type="checkbox" />
+          <span className="switch-left">Yes</span>
+          <span className="switch-right">No</span>
+        </label>
+        <label className="rocker rocker-small">
+          <input type="checkbox" />
+          <span className="switch-left">Yes</span>
+          <span className="switch-right">No</span>
+        </label>
+      </div>
+      <Switch
+        onChange={toggleTheme}
+        checked={theme === 'dark'}
+        height={24}
+        width={48}
+        checkedIcon={<Icon type="moon" />}
+        uncheckedIcon={<Icon type="sun" />}
+        offColor={'#d9dfe2'}
+        offHandleColor={'#fff'}
+        onColor={'#999'}
+        onHandleColor={'#282c35'}
+      />
+    </>
+  )
+}

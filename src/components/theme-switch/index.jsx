51a2ba5c335c34185587dd51f8138b383ba2e2af
춤추는망고 <ensia96@gmail.com>
2021-02-04 21:25:21 +0900

    [add] prop delivering to styled component

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -57,39 +57,34 @@ const Switch = ({ children, position, checked }) => {
       transform: skewY(-65deg);
     }
 
-    background-color: ${left
-      ? checked
-        ? '#0084d0'
-        : '#ddd'
-      : checked
-      ? '#ddd'
-      : '#bd5757'};
-    bottom: ${left ? (checked ? '0px' : '0.4em') : checked ? '0.4em' : '0'};
-    ${left
-      ? `left: ${checked ? '0.5em' : '0.85em'}`
-      : `right: ${checked ? '0.8em' : '0.5em'}`};
-    height: ${left
-      ? checked
-        ? '2.5em'
-        : '2.4em'
-      : checked
-      ? '2.4em'
-      : '2.5em'};
-    width: ${left ? (checked ? '3em' : '2.75em') : checked ? '2.75em' : '3em'};
-    transform: ${left
-      ? checked
-        ? 'rotate(0deg) skewX(0deg)'
-        : 'rotate(15deg) skewX(15deg)'
-      : checked
-      ? 'rotate(-15deg) skewX(-15deg)'
-      : 'rotate(0deg) skewX(0deg)'};
+    background-color: ${({ left, checked }) =>
+      left ? (checked ? '#0084d0' : '#ddd') : checked ? '#ddd' : '#bd5757'};
+    bottom: ${({ left, checked }) =>
+      left ? (checked ? '0px' : '0.4em') : checked ? '0.4em' : '0'};
+    ${({ left, checked }) =>
+      left
+        ? `left: ${checked ? '0.5em' : '0.85em'}`
+        : `right: ${checked ? '0.8em' : '0.5em'}`};
+    height: ${({ left, checked }) =>
+      left ? (checked ? '2.5em' : '2.4em') : checked ? '2.4em' : '2.5em'};
+    width: ${({ left, checked }) =>
+      left ? (checked ? '3em' : '2.75em') : checked ? '2.75em' : '3em'};
+    transform: ${({ left, checked }) =>
+      left
+        ? checked
+          ? 'rotate(0deg) skewX(0deg)'
+          : 'rotate(15deg) skewX(15deg)'
+        : checked
+        ? 'rotate(-15deg) skewX(-15deg)'
+        : 'rotate(0deg) skewX(0deg)'};
 
     ::before {
-      ${left
-        ? `
+      ${({ left, checked }) =>
+        left
+          ? `
 ${checked ? 'background-color: transparent; width: 3.0833em;' : 'left: -0.4em;'}
 `
-        : `right: ${checked ? '-0.4em' : '-0.375em'};
+          : `right: ${checked ? '-0.4em' : '-0.375em'};
   background-color: ${checked ? '#ccc' : 'transparent'};
   transform: skewY(65deg);`}
     }

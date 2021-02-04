c1b521aeebab533240e04808a49c5c4bf1f306c8
춤추는망고 <ensia96@gmail.com>
2021-02-04 17:46:34 +0900

    [create] container styled component

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -3,13 +3,38 @@ import Switch from 'react-switch'
 
 import Icon from './icon'
 
+import styled from 'styled-components'
+
 import './index.scss'
+
+const Container = styled.label`
+  position: relative;
+  text-align: center;
+  color: #888;
+  width: 7em;
+  height: 4em;
+  overflow: hidden;
+  border-bottom: 0.5em solid #eee;
+`
 
 export default ({ theme, toggleTheme }) => {
   console.log('theme : ', theme)
 
   return (
     <>
+      <Container>
+        <input
+          type="checkbox"
+          checked={theme === 'light'}
+          onChange={toggleTheme}
+        />
+        <span className="switch-left">
+          <Icon type="sun" />
+        </span>
+        <span className="switch-right">
+          <Icon type="moon" />
+        </span>
+      </Container>
       <label className="rocker">
         <input
           type="checkbox"

f432c2c92fe5941816b9716e77e0ce780ca683bb
춤추는망고 <ensia96@gmail.com>
2021-02-04 21:26:56 +0900

    [change] switch component to styled component

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -34,68 +34,58 @@ const Input = styled.input`
   height: 0;
 `
 
-const Switch = ({ children, position, checked }) => {
-  const left = position === 'left'
+const Switch = styled.span`
+  cursor: pointer;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 2.5em;
+  width: 3em;
+  transition: 0.2s;
 
-  const Component = styled.span`
-    cursor: pointer;
+  ::before {
+    content: '';
     position: absolute;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 2.5em;
-    width: 3em;
-    transition: 0.2s;
+    width: 0.4em;
+    height: 2.45em;
+    bottom: -0.45em;
+    background-color: #ccc;
+    transform: skewY(-65deg);
+  }
 
-    ::before {
-      content: '';
-      position: absolute;
-      width: 0.4em;
-      height: 2.45em;
-      bottom: -0.45em;
-      background-color: #ccc;
-      transform: skewY(-65deg);
-    }
+  background-color: ${({ left, checked }) =>
+    left ? (checked ? '#0084d0' : '#ddd') : checked ? '#ddd' : '#bd5757'};
+  bottom: ${({ left, checked }) =>
+    left ? (checked ? '0px' : '0.4em') : checked ? '0.4em' : '0'};
+  ${({ left, checked }) =>
+    left
+      ? `left: ${checked ? '0.5em' : '0.85em'}`
+      : `right: ${checked ? '0.8em' : '0.5em'}`};
+  height: ${({ left, checked }) =>
+    left ? (checked ? '2.5em' : '2.4em') : checked ? '2.4em' : '2.5em'};
+  width: ${({ left, checked }) =>
+    left ? (checked ? '3em' : '2.75em') : checked ? '2.75em' : '3em'};
+  transform: ${({ left, checked }) =>
+    left
+      ? checked
+        ? 'rotate(0deg) skewX(0deg)'
+        : 'rotate(15deg) skewX(15deg)'
+      : checked
+      ? 'rotate(-15deg) skewX(-15deg)'
+      : 'rotate(0deg) skewX(0deg)'};
 
-    background-color: ${({ left, checked }) =>
-      left ? (checked ? '#0084d0' : '#ddd') : checked ? '#ddd' : '#bd5757'};
-    bottom: ${({ left, checked }) =>
-      left ? (checked ? '0px' : '0.4em') : checked ? '0.4em' : '0'};
+  ::before {
     ${({ left, checked }) =>
       left
-        ? `left: ${checked ? '0.5em' : '0.85em'}`
-        : `right: ${checked ? '0.8em' : '0.5em'}`};
-    height: ${({ left, checked }) =>
-      left ? (checked ? '2.5em' : '2.4em') : checked ? '2.4em' : '2.5em'};
-    width: ${({ left, checked }) =>
-      left ? (checked ? '3em' : '2.75em') : checked ? '2.75em' : '3em'};
-    transform: ${({ left, checked }) =>
-      left
-        ? checked
-          ? 'rotate(0deg) skewX(0deg)'
-          : 'rotate(15deg) skewX(15deg)'
-        : checked
-        ? 'rotate(-15deg) skewX(-15deg)'
-        : 'rotate(0deg) skewX(0deg)'};
-
-    ::before {
-      ${({ left, checked }) =>
-        left
-          ? `
+        ? `
 ${checked ? 'background-color: transparent; width: 3.0833em;' : 'left: -0.4em;'}
 `
-          : `right: ${checked ? '-0.4em' : '-0.375em'};
+        : `right: ${checked ? '-0.4em' : '-0.375em'};
   background-color: ${checked ? '#ccc' : 'transparent'};
   transform: skewY(65deg);`}
-    }
-  `
-
-  return (
-    <Component left={left} checked={checked}>
-      {children}
-    </Component>
-  )
-}
+  }
+`
 
 export default ({ theme, toggleTheme }) => {
   const checked = theme === 'light'
@@ -104,10 +94,10 @@ export default ({ theme, toggleTheme }) => {
     <>
       <Container>
         <Input type="checkbox" checked={checked} onChange={toggleTheme} />
-        <Switch position="left" checked={checked}>
+        <Switch left checked={checked}>
           <Icon type="sun" />
         </Switch>
-        <Switch position="right" checked={checked}>
+        <Switch checked={checked}>
           <Icon type="moon" />
         </Switch>
       </Container>

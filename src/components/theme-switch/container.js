27244e907c6896b7f4d8a5752491e32b69a63d33
춤추는망고 <ensia96@gmail.com>
2021-02-05 10:53:56 +0900

    [create] container component module

---
+++ src/components/theme-switch/container.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export default styled.label`
+  display: flex;
+  text-align: center;
+  background-color: #888;
+  border: 3px solid #aaa;
+  border-radius: 5%;
+  padding: 0 1px;
+`

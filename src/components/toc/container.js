b91936bb0eefabd7b52de1b741a7977223493875
춤추는망고 <ensia96@gmail.com>
2021-02-10 10:51:43 +0900

    deployment/debug(#74) (#79)

--- src/components/toc/container.js
+++ src/components/toc/container.js
@@ -3,12 +3,13 @@ import styled from 'styled-components'
 export default styled.nav`
   z-index: 1;
   position: fixed;
-  display: inline-block;
 
+  top: 28%;
   right: ${({ open }) => (open ? 0 : '-200px')};
   transition: right 0.5s;
 
   width: 200px;
+  height: 300px;
 
   background-color: ${({ theme: { background } }) => background};
 
@@ -17,13 +18,16 @@ export default styled.nav`
 
   border: 1px solid #dddddd;
 
-  overflow: scroll;
-  white-space: nowrap;
-
   padding: 10px;
 
   font-size: 14px;
   font-family: Catamaran;
+
+  div {
+    height: 100%;
+    overflow: scroll;
+    white-space: nowrap;
+  }
 
   ul {
     list-style: none;

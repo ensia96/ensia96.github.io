854218ef11a890687e6c08272cc8895b65240184
춤추는망고 <ensia96@gmail.com>
2021-02-13 20:07:58 +0900

    [change] global style to use tag style

--- src/layout/global.js
+++ src/layout/global.js
@@ -1,42 +1,51 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
+* {
+    margin: 0;
+    padding: 0;
 
+    color: ${({ theme: { font } }) => font};
+    text-decoration: none;
+    &:hover {
+        text-decoration: none;
+    }
+
+    a {
         color: ${({ theme: { font } }) => font};
         text-decoration: none;
         &:hover {
             text-decoration: none;
         }
-
-        a {
-            color: ${({ theme: { font } }) => font};
-            text-decoration: none;
-            &:hover {
-                text-decoration: none;
-            }
-        }
-
-        code {
-            padding: 0.1em 0.6em;
-            border-radius: 0.2em;
-            white-space: normal;
-            background: #ffe887;
-            color: #c7a200;
-        }
     }
-    html, body {
-        overflow: auto;
 
-        font-family: 'Noto Sans KR', sans-serif;
-        background-color: ${({ theme: { background } }) => background};
-        -webkit-text-size-adjust: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-
-        div[role='group'] {
-            -webkit-overflow-scrolling: touch;
-        }
+    code {
+        padding: 0.1em 0.6em;
+        border-radius: 0.2em;
+        white-space: normal;
+        background: ${({
+          theme: {
+            tag: { background },
+          },
+        }) => background};
+        color: ${({
+          theme: {
+            tag: { font },
+          },
+        }) => font};
     }
+}
+html, body {
+    overflow: auto;
+
+    font-family: 'Noto Sans KR', sans-serif;
+    background-color: ${({ theme: { background } }) => background};
+    -webkit-text-size-adjust: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    div[role='group'] {
+        -webkit-overflow-scrolling: touch;
+    }
+
+}
 `

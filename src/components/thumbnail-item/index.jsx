4a4f2b62b6bb731c4626f5366009bca97c9598fb
춤추는망고 <ensia96@gmail.com>
2021-02-13 19:46:50 +0900

    [add] tag rendering code to thumbnail item component

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -5,12 +5,17 @@ import Container from './container'
 export default ({
   node: {
     fields: { slug },
-    frontmatter: { title },
+    frontmatter: { title, tags },
     excerpt,
   },
 }) => (
   <Container to={slug}>
     <h3>{title || slug}</h3>
     <p dangerouslySetInnerHTML={{ __html: excerpt }} />
+    {tags ? (
+      tags.map(item => <code style={{ margin: '5px' }}># {item}</code>)
+    ) : (
+      <div />
+    )}
   </Container>
 )

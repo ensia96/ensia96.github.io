4011bbf59e0129dc0e4657ee015ffeb9114d95cf
춤추는망고 <ensia96@gmail.com>
2021-02-28 16:27:14 +0900

    maintanance/#82 (#89)

--- src/components/tags/index.jsx
+++ src/components/tags/index.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Tag = styled.code`
+  border-radius: 0.2em;
+
   margin: 5px;
+  padding: 0.1em 0.6em;
 `
 
 export default ({ tags }) => (

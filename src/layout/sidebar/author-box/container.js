95ddf55e9142c13fec1b12a6a2cb0e27975beb01
춤추는망고 <ensia96@gmail.com>
2021-07-13 19:04:04 +0900

    [change] configuration of author box component

---
+++ src/layout/sidebar/author-box/container.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  height: 50px;
+  cursor: pointer;
+  font-size: 18px;
+`
--- src/layout/sidebar/author-box/index.jsx
+++ src/layout/sidebar/author-box/index.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
-import styled from 'styled-components'
 
 import Image from 'gatsby-image'
 
+import Container from './container.js'
 import GreenDot from './greendot.js'
 import Avatar from '../avatar'
 
-const AuthorBox = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 25px;
-  height: 50px;
-  cursor: pointer;
-  font-size: 18px;
-`
-
 export default ({ author, avatar, onClick }) => (
-  <AuthorBox onClick={onClick}>
+  <Container onClick={onClick}>
     <Avatar size={27} fixed={avatar} />
     {author}
     <GreenDot />
-  </AuthorBox>
+  </Container>
 )

56915a8ff4a6596a182c303daa5cdfea8673e7a2
춤추는망고 <ensia96@gmail.com>
2021-07-13 17:37:37 +0900

    [change] url parameter to get url parameter function

--- src/utils/storage/browser.js
+++ src/utils/storage/browser.js
@@ -3,8 +3,9 @@ const win = typeof window !== `undefined` ? window : {}
 export const localStorage = win.localStorage
 export const sessionStorage = win.sessionStorage
 export const innerWidth = win.innerWidth
-export const urlParameter = win.location.search
-  .substr(1)
-  .split('&')
-  .map(query => query.split('='))
-  .reduce((object, [key, value]) => (object[key] = value) && object, {})
+export const getUrlParameter = () =>
+  win.location.search
+    .substr(1)
+    .split('&')
+    .map(query => query.split('='))
+    .reduce((object, [key, value]) => (object[key] = value) && object, {}) || {}

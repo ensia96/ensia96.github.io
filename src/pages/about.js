7efa33568442c0a3b47734770d562937de92e056
춤추는망고 <ensia96@gmail.com>
2021-08-12 16:51:00 +0900

    [add] avatar component to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../layout'
+import Avatar from '../layout/sidebar/avatar'
 
 export default ({
   location,
   data: {
+    avatar: {
+      childImageSharp: { fixed: avatar },
+    },
     site: {
       siteMetadata: { title },
     },
@@ -13,6 +17,7 @@ export default ({
   },
 }) => (
   <Layout location={location} title={title}>
+    <Avatar size={300} fixed={avatar} />
     <div
       dangerouslySetInnerHTML={{
         __html: resumes.map(({ node }) => node['html']).join('<br>'),
@@ -23,6 +28,13 @@ export default ({
 
 export const pageQuery = graphql`
   query {
+    avatar: file(absolutePath: { regex: "/resume.png/" }) {
+      childImageSharp {
+        fixed(width: 300, height: 300) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
     site {
       siteMetadata {
         title

0ace096294a5b4c9e3be347283a951eda11694ba
춤추는망고 <ensia96@gmail.com>
2021-08-12 16:53:18 +0900

    [create] components to make self introduction area

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import styled from 'styled-components'
+
 import Layout from '../layout'
 import Avatar from '../layout/sidebar/avatar'
+
+const A = styled.div`
+  @media all and (min-width: 1260px) {
+    display: inline-flex;
+  }
+`
+const B = styled.div`
+  @media all and (min-width: 1260px) {
+    margin-left: 30px;
+  }
+`
 
 export default ({
   location,
@@ -15,16 +28,26 @@ export default ({
     },
     allMarkdownRemark: { edges: resumes },
   },
-}) => (
-  <Layout location={location} title={title}>
-    <Avatar size={300} fixed={avatar} />
-    <div
-      dangerouslySetInnerHTML={{
-        __html: resumes.map(({ node }) => node['html']).join('<br>'),
-      }}
-    />
-  </Layout>
-)
+}) => {
+  const html = resumes.map(({ node }) => node['html'])
+  return (
+    <Layout location={location} title={title}>
+      <A>
+        <Avatar size={300} fixed={avatar} />
+        <B
+          dangerouslySetInnerHTML={{
+            __html: html[0],
+          }}
+        />
+      </A>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: html.slice(1).join('<br>'),
+        }}
+      />
+    </Layout>
+  )
+}
 
 export const pageQuery = graphql`
   query {

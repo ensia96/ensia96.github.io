aa527d7ddf4af5b3bb65618721f67458a41d5cc2
춤추는망고 <ensia96@gmail.com>
2021-08-12 18:02:46 +0900

    [delete] content component of introduction component

--- src/components/introduction/content.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.div`
-  @media all and (min-width: 1260px) {
-    margin-left: 30px;
-  }
-`
--- src/components/introduction/index.jsx
+++ src/components/introduction/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import Container from './container'
-import Content from './content'
 
 import Avatar from '../../layout/sidebar/avatar'
 
 export default ({ avatar, html: __html }) => (
   <Container>
     <Avatar size={300} fixed={avatar} />
-    <Content dangerouslySetInnerHTML={{ __html }} />
+    <div dangerouslySetInnerHTML={{ __html }} />
   </Container>
 )

bd3f16cce8cfcf2316be0bf1588ed64ab5b19488
춤추는망고 <ensia96@gmail.com>
2021-08-12 17:58:58 +0900

    [change] style in introduction container for better grid rendering

--- src/components/introduction/container.js
+++ src/components/introduction/container.js
@@ -2,7 +2,9 @@ import styled from 'styled-components'
 
 export default styled.div`
   @media all and (min-width: 1260px) {
-    display: inline-grid;
+    display: grid;
     grid-auto-flow: column;
+    grid-gap: 30px;
+    grid-template-columns: max-content;
   }
 `

0cb7a6530494eb38b403d54d8fcd3b02f26da750
춤추는망고 <ensia96@gmail.com>
2021-08-23 16:32:48 +0900

    [change] style of tag component

--- src/components/tags/index.jsx
+++ src/components/tags/index.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Tag = styled.code`
-  border-radius: 0.2em;
+  border-radius: 1em;
+  border-style: solid;
+  border-width: 2px;
+
+  white-space: pre;
+  word-break: normal;
+  word-wrap: normal;
 
   margin: 5px;
   padding: 0.1em 0.6em;

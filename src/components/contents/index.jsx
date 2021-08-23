531b02d4f2e449c59dab293f5eb413bdbed5e560
춤추는망고 <ensia96@gmail.com>
2021-08-23 16:38:54 +0900

    [add] content filtering by tag

--- src/components/contents/index.jsx
+++ src/components/contents/index.jsx
@@ -3,10 +3,14 @@ import React, { useMemo } from 'react'
 import ThumbnailContainer from '../thumbnail-container'
 import ThumbnailItem from '../thumbnail-item'
 
-export default ({ posts, countOfInitialPost, count, path }) => {
+export default ({ posts, countOfInitialPost, count, path, tag }) => {
   const contents = useMemo(() =>
     (path
       ? posts.filter(({ node }) => node.fields.slug.includes(decodeURI(path)))
+      : tag
+      ? posts.filter(({ node }) =>
+          node.frontmatter.tags.includes(decodeURI(tag))
+        )
       : posts.filter(({ node }) => !node.fields.slug.includes('TIL'))
     ).slice(0, count * countOfInitialPost)
   )

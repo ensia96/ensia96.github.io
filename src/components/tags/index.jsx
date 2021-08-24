597fc343ebbeb38a1cf5329e6c5e17b45c6d7594
춤추는망고 <ensia96@gmail.com>
2021-08-24 15:08:40 +0900

    [change] config of tags component

--- src/components/tags/index.jsx
+++ src/components/tags/index.jsx
@@ -1,30 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
 
-import { Link } from 'gatsby'
-
-const Container = styled(Link)`
-  padding: 0 !important;
-`
-
-const Tag = styled.code`
-  border-radius: 1em;
-  border-style: solid;
-  border-width: 2px;
-
-  display: inline-block;
-
-  line-height: normal;
-
-  margin: 0.1em 0.3em;
-  padding: 0.1em 0.6em;
-`
+import Container from './container'
+import Item from './item'
 
 export default ({ tags }) => (
   <>
     {tags?.map((item, i) => (
       <Container key={i} to={`/?tag=${item}`}>
-        <Tag># {item}</Tag>
+        <Item># {item}</Item>
       </Container>
     ))}
   </>

f52277d22d4c25e0e59413bed9d373e2c901ff9b
춤추는망고 <ensia96@gmail.com>
2021-08-24 14:55:22 +0900

    maintanance/#135 (#136)

--- src/components/tags/index.jsx
+++ src/components/tags/index.jsx
@@ -1,27 +1,14 @@
 import React from 'react'
-import styled from 'styled-components'
 
-import { Link } from 'gatsby'
-
-const Tag = styled.code`
-  border-radius: 1em;
-  border-style: solid;
-  border-width: 2px;
-
-  white-space: pre;
-  word-break: normal;
-  word-wrap: normal;
-
-  margin: 5px;
-  padding: 0.1em 0.6em;
-`
+import Container from './container'
+import Item from './item'
 
 export default ({ tags }) => (
   <>
     {tags?.map((item, i) => (
-      <Link key={i} to={`/?tag=${item}`}>
-        <Tag># {item}</Tag>
-      </Link>
+      <Container key={i} to={`/?tag=${item}`}>
+        <Item># {item}</Item>
+      </Container>
     ))}
   </>
 )
---
+++ src/components/tags/item.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default styled.code`
+  border-radius: 1em;
+  border-style: solid;
+  border-width: 2px;
+
+  display: inline-block;
+
+  line-height: normal;
+
+  margin: 0.1em 0.3em;
+  padding: 0.1em 0.6em;
+`

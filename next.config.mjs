32b4545941657e18b6f81649154dea8b9e92b1e8
춤추는망고 <ensia96@gmail.com>
2024-06-30 01:59:09 +0900

    feat: mdx 관련 설정 추가 (nextConfig)

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
-// output, basePath, and images are required for GitHub Pages.
-// See https://github.com/gregrickaby/nextjs-github-pages
+import withMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // output, basePath, and images are required for GitHub Pages.
+  // See https://github.com/gregrickaby/nextjs-github-pages
   output: "export",
   // TODO(hojoon): replace repository name and change base path
   basePath: "/blog",
   images: {
     unoptimized: true,
   },
+
+  // Configure `pageExtensions` to include MDX files
+  // See https://nextjs.org/docs/app/building-your-application/configuring/mdx#getting-started
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
 };
 
-export default nextConfig;
+export default withMDX()(nextConfig);

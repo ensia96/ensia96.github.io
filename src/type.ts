ae0b525ed673b856a0abcefcb4bbfbe647167190
춤추는망고 <ensia96@gmail.com>
2024-07-12 19:54:37 +0900

    chore: 주석 추가 (type)

--- src/type.ts
+++ src/type.ts
@@ -1,3 +1,19 @@
+// ======================
+// data
+// ======================
+
+// export type SomeData = {};
+
+// ======================
+// function
+// ======================
+
 export type Request = <Response>(input: RequestInfo | URL) => Promise<Response>;
 
 export type JoinUrl = (...args: string[]) => string;
+
+// ======================
+// api
+// ======================
+
+// export type SomeRequestResponse = {};

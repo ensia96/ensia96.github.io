913c2250da2c1780c1f6fbe29b4f59214d5e70f4
춤추는망고 <ensia96@gmail.com>
2025-02-22 16:30:23 +0900

    feat: 내용 입력 기능 추가 (New)

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,9 +1,39 @@
 "use client";
+import { useState } from "react";
 
 const New = () => {
+  const [state, setState] = useState<NewState>({
+    commitMessage: "",
+    content: "",
+  });
+
   const submit: React.ComponentProps<"form">["onSubmit"] = async (event) => {};
 
-  return <form {...{ onSubmit: submit }}></form>;
+  const updateCommitMessage: React.ComponentProps<"input">["onChange"] = (
+    event
+  ) =>
+    setState((state) => ({
+      ...state,
+      commitMessage: event.target.value,
+    }));
+
+  const updateContent: React.ComponentProps<"textarea">["onChange"] = (event) =>
+    setState((state) => ({ ...state, content: event.target.value }));
+
+  return (
+    <form {...{ onSubmit: submit }}>
+      <input
+        {...{ onChange: updateCommitMessage, value: state.commitMessage }}
+      />
+
+      <textarea {...{ onChange: updateContent, value: state.content }} />
+    </form>
+  );
 };
+
+interface NewState {
+  commitMessage: string;
+  content: string;
+}
 
 export default New;
